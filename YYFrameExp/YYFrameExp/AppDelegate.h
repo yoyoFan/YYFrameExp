9a9fb3342227c2ad2026427bcf96d43d01f989ef
fwr <fanwr@etuo.cn>
2016-04-21 20:18:49 +0800

    fwr s

---
+++ YYFrameExp/YYFrameExp/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  YYFrameExp
+//
+//  Created by fwr on 16/4/21.
+//  Copyright © 2016年 com.YoYo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ YYFrameExp/YYFrameExp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YYFrameExp
+//
+//  Created by fwr on 16/4/21.
+//  Copyright © 2016年 com.YoYo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
