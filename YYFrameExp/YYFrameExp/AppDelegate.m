d0e47f0415f43622d03e17a75d88f037079277d5
fwr <fanwr@etuo.cn>
2016-04-21 20:25:31 +0800

    注释

--- YYFrameExp/YYFrameExp/AppDelegate.m
+++ YYFrameExp/YYFrameExp/AppDelegate.m
@@ -17,6 +17,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    //我建的项目 
+    
+    
+    
     return YES;
 }
 
